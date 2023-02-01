bf4a7ca4f80763383e0b30304550d2aaeb6ffe12
makexcake <116571218+makexcake@users.noreply.github.com>
2023-02-01 14:44:05 +0200

    updated eks-cluster.tf

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -31,7 +31,7 @@ module "eks" {
 
 
 output "list_of_engs" {
-  value = module.eks.eks_managed_node_groups
+  value = module.eks.eks_managed_node_groups["dev"]
 }
 
 /*resource "aws_iam_policy" "csi-driver-policy" {
